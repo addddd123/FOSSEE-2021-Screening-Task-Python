e5cd3eb4b4a66c80fbe59199762e0dab058419bb
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2021-04-10 11:59:19 -0700

    Add files via upload
    for orginal intern

---
+++ checking.bat
@@ -0,0 +1,37 @@
+@echo off
+set current_path=%~dp0
+call pip install %current_path%python_dateutil-2.8.1-py2.py3-none-any.whl
+call pip install %current_path%chardet-4.0.0-py2.py3-none-any.whl
+call pip install %current_path%idna-2.10-py2.py3-none-any.whl
+call pip install %current_path%urllib3-1.26.4-py2.py3-none-any.whl
+call pip install %current_path%certifi-2020.12.5-py2.py3-none-any.whl
+call pip install %current_path%requests-2.25.1-py2.py3-none-any.whl
+call pip install %current_path%attrs-18.2.0-py2.py3-none-any.whl
+call pip install %current_path%cachy-0.3.0-py2.py3-none-any.whl
+call pip install %current_path%zipp-3.4.1-py3-none-any.whl
+call pip install %current_path%pywin32_ctypes-0.2.0-py2.py3-none-any.whl
+call pip install %current_path%importlib_metadata-3.10.0-py3-none-any.whl
+call pip install %current_path%typing_extensions-3.7.4.3-py3-none-any.whl
+call pip install %current_path%PyQt5_sip-12.8.1-cp37-cp37m-win_amd64.whl
+call pip install %current_path%PyQt5-5.14.2-5.14.2-cp35.cp36.cp37.cp38-none-win_amd64.whl
+call pip install %current_path%PyQt5_Qt5-5.15.2-py3-none-win_amd64.whl
+call conda install %current_path%python-pdfkit-0.6.1-py_0.tar.bz2
+call conda install %current_path%pyyaml-5.4.1-py37hcc03f2d_0.tar.bz2
+call pip install %current_path%Pygments-2.8.1-py3-none-any.whl
+call pip install %current_path%XlsxWriter-1.2.8.tar.gz
+call pip install %current_path%ordered-set-4.0.2.tar.gz
+call pip install %current_path%PyLaTeX-1.4.1.tar.gz
+call pip install %current_path%attrs-18.2.0-py2.py3-none-any.whl
+call pip install %current_path%pdflatex-0.1.3-py3-none-any.whl
+call pip install %current_path%pdfkit-0.6.1-py3-none-any.whl
+call pip install %current_path%keyring-23.0.1-py3-none-any.whl
+call pip install %current_path%Deprecated-1.2.12-py2.py3-none-any.whl
+call pip install %current_path%wrapt-1.12.1.tar.gz
+call pip install %current_path%PyJWT-1.7.1-py2.py3-none-any.whl
+call pip install %current_path%PyGithub-1.54.1-py3-none-any.whl
+call pip install %current_path%pytz-2021.1-py2.py3-none-any.whl
+call pip install %current_path%python_dateutil-2.8.1-py2.py3-none-any.whl
+call pip install %current_path%et_xmlfile-1.0.1.tar.gz
+call pip install %current_path%openpyxl-3.0.7-py2.py3-none-any.whl
+call pip install %current_path%pandas-1.2.3-cp37-cp37m-win_amd64.whl
+pause
